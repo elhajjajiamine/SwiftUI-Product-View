831835053d5ad042c444c10218231f6ba7bcdb78
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-16 12:07:54 +0100

    Initial Commit

---
+++ SwiftUI Product View/SwiftUI_Product_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Product_ViewApp.swift
+//  SwiftUI Product View
+//
+//  Created by elhajjaji on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Product_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
